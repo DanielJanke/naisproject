4acaaba3af960a7b91b5a5259fdcf6bf547e2a49
Daniel Janke <Daniel.Janke@me.com>
2018-01-19 19:00:06 +0100

    added basic map

--- map/src/app/components/map/map.component.ts
+++ map/src/app/components/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
+import * as mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
 
 @Component({
   selector: 'app-map',
@@ -10,6 +12,15 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+
+ 
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsamFua2UiLCJhIjoiY2pjbTc5c2M5MDgxMjJxczUxN2RrZ3RuNSJ9.0q10LXe5ZgRrJbubAXpzNQ';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+    });
   }
 
 }
