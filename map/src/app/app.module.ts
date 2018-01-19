faecae169770a022d4b273a2896f3eb2800a89ec
Vincent Rohde <vincent.rohde@icloud.com>
2018-01-19 18:16:56 +0100

    Add components, map, navbar

--- map/src/app/app.module.ts
+++ map/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MapComponent } from './components/map/map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule
